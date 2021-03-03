e9e4f482fdb21b62734ec36f8b3a0dab45047768
igeo <georgeisaev@gmail.com>
2021-03-03 23:09:20 +0700

    edject configs, add pho bo components

---
+++ src/containers/PhoboBuilder/PhoboBuilder.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+
+class PhoboBuilder extends Component {
+  render() {
+    return <Aux>
+        <div>Pho bo</div>
+        <div>Build Controls</div>
+    </Aux>;
+  }
+}
+
+export default PhoboBuilder;
