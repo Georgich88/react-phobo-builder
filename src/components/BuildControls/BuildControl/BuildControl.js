9972594ba3b9545c57ecdaff05f12ce06f6b84bd
igeo <georgeisaev@gmail.com>
2021-04-03 08:13:23 +0700

    add order button

--- src/components/BuildControls/BuildControl/BuildControl.js
+++ src/components/BuildControls/BuildControl/BuildControl.js
@@ -3,6 +3,7 @@ import classes from "./BuildControl.css";
 
 const BuildControl = (props) => (
   <div className={classes.BuildControl}>
+    
     <div className={classes.Label}>{props.label}</div>
     <button
       className={classes.Less}
