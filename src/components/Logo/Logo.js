0c513bf9d4deca083b85ae848e554dcf76d8d95b
igeo <georgeisaev@gmail.com>
2021-04-12 14:22:27 +0700

    adding logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PhoboLogo from "../../assets/images/logo.png";
+import classes from "./Logo.css";
+
+const Logo = (props) => (
+  <div className={classes.Logo}>
+    <img src={PhoboLogo} alt="PhoboRadar"></img>
+  </div>
+);
+
+export default Logo;
