bf1e78d1f07647546f228bc3ffa3b074614b62ab
igeo <georgeisaev@gmail.com>
2021-04-15 13:43:25 +0700

    logo responsive adjustments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import PhoboLogo from "../../assets/images/phobo-logo.png";
 import classes from "./Logo.css";
 
 const Logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={{height:props.height}}>
     <img src={PhoboLogo} alt="PhoboRadar"></img>
   </div>
 );
